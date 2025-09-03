4a63a0bec3744b95d5389750c5c2c9d79f21a570
Danielle <danielle.shokrian@aol.com>
2025-09-03 12:22:55 -0400

    built a model to aid in building contracts for books with multiple authors.

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,56 @@
 class Author:
-    pass
+    all = []
+    def __init__(self, name):
+        self.name = name
+        Author.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+
+    def books(self):
+        return [contract.book for contract in self.contracts()]
+
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+    
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self.contracts())
 
 
 class Book:
-    pass
+    all = []
+    def __init__(self, title):
+        self.title = title
+        Book.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+    
+    def authors(self):
+        return [contract.author for contract in self.contracts()]
 
 
 class Contract:
-    pass
+    all = []
+    def __init__(self, author, book, date, royalties):
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        Contract.all.append(self)
+
+        if not isinstance(author, Author):
+            raise Exception("Invalid author")
+
+        if not isinstance(book, Book):
+            raise Exception("Invalid book")
+        
+        if not isinstance(date, str):
+            raise Exception("Invalid date")
+
+        if not isinstance(royalties, (int, float)):
+            raise Exception("Invalid royalties")
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contract for contract in cls.all if contract.date == date]
